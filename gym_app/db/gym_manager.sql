1a0511052f83a3a61a8861d1c7b9c971e3f9e60b
Derek Bracken <derekbracken@gmail.com>
2021-08-13 16:11:51 +0100

    Database set up

---
+++ gym_app/db/gym_manager.sql
@@ -0,0 +1,26 @@
+DROP TABLE sessions;
+DROP TABLE members;
+DROP TABLE bookings;
+
+
+CREATE TABLE members (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    premium BOOLEAN
+)
+
+CREATE TABLE sessions (
+    id  SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    description TEXT,
+    time VARCHAR(255),
+    day VARCHAR(255),
+    capacity INT
+)
+
+CREATE TABLE bookings (
+    id SERIAL PRIMARY KEY,
+    member_id INT REFERENCES members(id),
+    session_id INT REFERENCES sessions(id)
+)
+
