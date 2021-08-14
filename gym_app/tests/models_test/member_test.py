d800a431eb837b2e93d13325baf4a811ad3aed03
Derek Bracken <derekbracken@gmail.com>
2021-08-14 12:46:06 +0100

    member_test function placehoders added

--- gym_app/tests/models_test/member_test.py
+++ gym_app/tests/models_test/member_test.py
@@ -1,2 +1,26 @@
 import unittest
 from models.member import Member
+
+class TestMember(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    @unittest.skip("Delete this line to run the test")
+    def test_has_name(self):
+        pass
+
+    
+    @unittest.skip("Delete this line to run the test")
+    def test_has_premium_True(self):
+        pass
+
+
+    @unittest.skip("Delete this line to run the test")
+    def test_has_premium_False(self):
+        pass
+
+
+    @unittest.skip("Delete this line to run the test")
+    def test_has_id(self):
+        pass
