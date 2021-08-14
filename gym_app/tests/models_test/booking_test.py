8e9f1026867b3a440a1361c37023cb3e0a525315
Derek Bracken <derekbracken@gmail.com>
2021-08-14 13:13:46 +0100

    booking_test function placeholders added

--- gym_app/tests/models_test/booking_test.py
+++ gym_app/tests/models_test/booking_test.py
@@ -1,3 +1,20 @@
 import unittest
 from models.booking import Booking
 
+
+class TestBooking(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    @unittest.skip("Delete this to run the test")
+    def test_has_session(self):
+        pass
+
+    @unittest.skip("Delete this to run the test")
+    def test_has_member(self):
+        pass
+
+    @unittest.skip("Delete this to run the test")
+    def test_has_id(self):
+        pass
--- gym_app/tests/repositories_test/member_repository_test.py
+++ gym_app/tests/repositories_test/member_repository_test.py
@@ -1,2 +1,40 @@
 import unittest
-from repositories.member_repository import *
+import repositories.member_repository as member_repo
+
+
+class TestMemberRepository(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    @unittest.skip("Delete to run the test")
+    def test_save(self):
+        pass
+
+    @unittest.skip("Delete to run the test")
+    def test_select(self):
+        pass
+
+    @unittest.skip("Delete to run the test")
+    def test_select_all(self):
+        pass
+
+    @unittest.skip("Delete to run the test")
+    def test_delete(self):
+        pass
+
+    @unittest.skip("Delete to run the test")
+    def test_delete_all(self):
+        pass
+
+    @unittest.skip("Delete to run the test")
+    def test_update(self):
+        pass
+
+    @unittest.skip("Delete to run the test")
+    def test_sessions(self):
+        pass
+
+    @unittest.skip("Delete to run the test")
+    def test_save_member(self):
+        pass
