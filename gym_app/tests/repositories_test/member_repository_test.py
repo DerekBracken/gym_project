507569900f6399702a6266911d1409097d2eee81
Derek Bracken <derekbracken@gmail.com>
2021-08-14 20:01:23 +0100

    Added two member_repo tests

--- gym_app/tests/repositories_test/member_repository_test.py
+++ gym_app/tests/repositories_test/member_repository_test.py
@@ -1,5 +1,6 @@
 import unittest
 import repositories.member_repository as member_repo
+from models.member import Member
 
 
 class TestMemberRepository(unittest.TestCase):
@@ -7,9 +8,13 @@ class TestMemberRepository(unittest.TestCase):
     def setUp(self):
         pass
 
-    @unittest.skip("Delete to run the test")
-    def test_save(self):
-        pass
+    # @unittest.skip("Delete to run the test")
+    def test_save(self):  
+        member1 = Member("Kathryn McVitie")
+        member_repo.save(member1)
+        members = member_repo.select_all()  
+        member = member_repo.select(members[-1].id) 
+        self.assertEqual("Kathryn McVitie", member.name)
 
     @unittest.skip("Delete to run the test")
     def test_select(self):
@@ -19,9 +24,13 @@ class TestMemberRepository(unittest.TestCase):
     def test_select_all(self):
         pass
 
-    @unittest.skip("Delete to run the test")
+    # @unittest.skip("Delete to run the test")
     def test_delete(self):
-        pass
+        members_before = member_repo.select_all()  
+        member = member_repo.select(members_before[-1].id)
+        member_repo.delete(member.id)
+        members_after = member_repo.select_all()  
+        self.assertEqual(len(members_after), len(members_before)-1) 
 
     @unittest.skip("Delete to run the test")
     def test_delete_all(self):
