d7255288b3f61a3f15522a003da78653fc149d2f
Derek Bracken <derekbracken@gmail.com>
2021-08-14 13:29:47 +0100

    booking_repo_test function placeholders added

--- gym_app/tests/repositories_test/booking_repository_test.py
+++ gym_app/tests/repositories_test/booking_repository_test.py
@@ -1,2 +1,40 @@
 import unittest
-from repositories.booking_repository import *
+import repositories.booking_repository as booking_repo
+
+
+class TestBookingRepository(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    @unittest.skip("Delete to run the test")
+    def test_save(self):
+        pass
+
+    @unittest.skip("Delete to run the test")
+    def test_select(self):
+        pass
+
+    @unittest.skip("Delete to run the test")
+    def test_select_all(self):
+        pass
+
+    @unittest.skip("Delete to run the test")
+    def test_delete(self):
+        pass
+
+    @unittest.skip("Delete to run the test")
+    def test_delete_all(self):
+        pass
+
+    @unittest.skip("Delete to run the test")
+    def test_update(self):
+        pass
+
+    @unittest.skip("Delete to run the test")
+    def test_session(self):
+        pass
+
+    @unittest.skip("Delete to run the test")
+    def test_member(self):
+        pass
