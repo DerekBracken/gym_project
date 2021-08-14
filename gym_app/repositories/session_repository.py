95b2c41ee3520412cef91e5edd1201ef42110d21
Derek Bracken <derekbracken@gmail.com>
2021-08-14 12:53:24 +0100

    session_test function placehoders added

---
+++ gym_app/repositories/session_repository.py
@@ -0,0 +1,38 @@
+import unittest
+from unittest.case import TestCase
+from models.session import Session
+
+class TestSession(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+
+    @unittest.skip("Delete this line to run the test")
+    def test_has_name(self):
+        pass
+
+
+    @unittest.skip("Delete this line to run the test")
+    def test_has_description(self):
+        pass
+
+
+    @unittest.skip("Delete this line to run the test")
+    def test_has_time(self):
+        pass
+
+
+    @unittest.skip("Delete this line to run the test")
+    def test_has_day(self):
+        pass
+
+
+    @unittest.skip("Delete this line to run the test")
+    def test_has_capacity(self):
+        pass
+
+
+    @unittest.skip("Delete this line to run the test")
+    def test_has_id(self):
+        pass
