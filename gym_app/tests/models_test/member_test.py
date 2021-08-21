ae8c13ff230c78d96e2150efc1f93ca0cd69c1a4
Derek Bracken <derekbracken@gmail.com>
2021-08-21 15:21:50 +0100

    fixed the models tests

--- gym_app/tests/models_test/member_test.py
+++ gym_app/tests/models_test/member_test.py
@@ -5,12 +5,16 @@ from models.member import Member
 class TestMember(unittest.TestCase):
 
     def setUp(self):
-        self.member1 = Member("Cathal Wilson", True)
-        self.member2 = Member("Kathryn McVitie", False, 10)
+        self.member1 = Member("Cathal", "Wilson", True)
+        self.member2 = Member("Kathryn", "McVitie", False, 10)
 
     # @unittest.skip("Delete this line to run the test")
     def test_has_name(self):
-        self.assertEqual('Cathal Wilson', self.member1.name)
+        self.assertEqual('Cathal', self.member1.first_name)
+
+    # @unittest.skip("Delete this line to run the test")
+    def test_has_name(self):
+        self.assertEqual('Wilson', self.member1.last_name)
 
     # @unittest.skip("Delete this line to run the test")
     def test_has_premium_True(self):
@@ -23,7 +27,3 @@ class TestMember(unittest.TestCase):
     # @unittest.skip("Delete this line to run the test")
     def test_has_id(self):
         self.assertEqual(10, self.member2.id)
-
-    @unittest.skip("Delete this line to run the test")
-    def test_update_premium(self):
-        pass
